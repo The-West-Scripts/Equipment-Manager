06bb03cb3435f65be6968f4b2c795985d29e0182
Alin-Stefan Olaru <allenmcpotter@outlook.com>
2017-07-14 01:31:56 +0300

    Made the list update before opening

--- quicksetthing.user.js
+++ quicksetthing.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         xEquipment Manager
-// @version      0.1
+// @version      0.2
 // @description  A quick way of changing inventories in TW
 // @author       Alin "xShteff" Olaru
 // @website      https://xshteff.github.io
@@ -13,47 +13,53 @@
 var xEquipmentSets = {
     List: {},
     InitDropdown: function() {
-        var selectThing = new west.gui.Selectbox();
-        selectThing.setHeader("Equipment Sets");
-        for (var i = 0; i < EquipManager.list.length; i++) {
-            var match = 0;
-            ['body', 'neck', 'head', 'right_arm', 'left_arm', 'animal', 'pants', 'belt', 'yield', 'foot'].each(function(el) {
-                if (Wear.get(el) == null && EquipManager.list[i][el] == null)
-                    match++;
-                else if (Wear.get(el) != null && Wear.get(el).obj.item_id == EquipManager.list[i][el])
-                    match++;
-                else
-                    match = 0;
-            });
-            if (match != 10) {
-                var title = '&lt;div id=&quot;equipshow_' + EquipManager.list[i].equip_manager_id + '&quot; style=&quot;height: 120px; width: 160px; high&quot; &gt;';
-                title += EquipManager.titlepart(i, 'neck', null, 105);
-                title += EquipManager.titlepart(i, 'head', null, 65);
-                title += EquipManager.titlepart(i, 'right_arm', 45, null);
-                title += EquipManager.titlepart(i, 'body', 65, 65);
-                title += EquipManager.titlepart(i, 'left_arm', 45, 145);
-                title += EquipManager.titlepart(i, 'animal', 85, null);
-                title += EquipManager.titlepart(i, 'pants', 105, 65);
-                title += EquipManager.titlepart(i, 'belt', 65, 105);
-                title += EquipManager.titlepart(i, 'yield', 85, 145);
-                title += EquipManager.titlepart(i, 'foot', 105, 105);
-                title += '&lt;/div&gt;';
-                var label = "<span title='" + title + "'>" + EquipManager.list[i].name + "</span>";
-                selectThing.addItem(i, label);
+        Ajax.remoteCallMode('inventory', 'show_equip', {}, function(data) {
+            EquipManager.list = data.data;
+            EquipManager.max = data.max;
+            EquipManager.premiumMax = data.premium_max;
+            EquipManager.hasPremium = data.hasPremium;
+            var selectThing = new west.gui.Selectbox();
+            selectThing.setHeader("Equipment Sets");
+            for (var i = 0; i < EquipManager.list.length; i++) {
+                var match = 0;
+                ['body', 'neck', 'head', 'right_arm', 'left_arm', 'animal', 'pants', 'belt', 'yield', 'foot'].each(function(el) {
+                    if (Wear.get(el) == null && EquipManager.list[i][el] == null)
+                        match++;
+                    else if (Wear.get(el) != null && Wear.get(el).obj.item_id == EquipManager.list[i][el])
+                        match++;
+                    else
+                        match = 0;
+                });
+                if (match != 10) {
+                    var title = '&lt;div id=&quot;equipshow_' + EquipManager.list[i].equip_manager_id + '&quot; style=&quot;height: 120px; width: 160px; high&quot; &gt;';
+                    title += EquipManager.titlepart(i, 'neck', null, 105);
+                    title += EquipManager.titlepart(i, 'head', null, 65);
+                    title += EquipManager.titlepart(i, 'right_arm', 45, null);
+                    title += EquipManager.titlepart(i, 'body', 65, 65);
+                    title += EquipManager.titlepart(i, 'left_arm', 45, 145);
+                    title += EquipManager.titlepart(i, 'animal', 85, null);
+                    title += EquipManager.titlepart(i, 'pants', 105, 65);
+                    title += EquipManager.titlepart(i, 'belt', 65, 105);
+                    title += EquipManager.titlepart(i, 'yield', 85, 145);
+                    title += EquipManager.titlepart(i, 'foot', 105, 105);
+                    title += '&lt;/div&gt;';
+                    var label = "<span title='" + title + "'>" + EquipManager.list[i].name + "</span>";
+                    selectThing.addItem(i, label);
 
+                }
+                xEquipmentSets.List[i] = EquipManager.list[i].equip_manager_id;
             }
-            xEquipmentSets.List[i] = EquipManager.list[i].equip_manager_id;
-        }
-        selectThing.setWidth(200);
-        selectThing.addListener(function(x) {
-            EquipManager.switchEquip(xEquipmentSets.List[x]);
-            new UserMessage("Equipment replaced successfully!", UserMessage.TYPE_SUCCESS).show();
+            selectThing.setWidth(200);
+            selectThing.addListener(function(x) {
+                EquipManager.switchEquip(xEquipmentSets.List[x]);
+                new UserMessage("Equipment replaced successfully!", UserMessage.TYPE_SUCCESS).show();
+            });
+            var pos = $('#xequip_init_button').offset();
+            pos.left -= 225;
+            selectThing.divWrap.offset(pos);
+            selectThing.divWrap.css('position', 'absolute');
+            selectThing.show();
         });
-        var pos = $('#xequip_init_button').offset();
-        pos.left -= 225;
-        selectThing.divWrap.offset(pos);
-        selectThing.divWrap.css('position', 'absolute');
-        selectThing.show();
     },
     InitButton: function() {
         var icon = $('<div></div>').attr({
@@ -98,12 +104,6 @@ var xEquipmentSets = {
         window.scriptyscript.init();
     },
     Init: function() {
-        Ajax.remoteCallMode('inventory', 'show_equip', {}, function(data) {
-            EquipManager.list = data.data;
-            EquipManager.max = data.max;
-            EquipManager.premiumMax = data.premium_max;
-            EquipManager.hasPremium = data.hasPremium;
-        });
         xEquipmentSets.InitButton();
         xEquipmentSets.RegisterToTWAPI();
     }
